fc066099169ac70d5a45e8ab078579425c8a87ef
Haydn Dias <haydnneildias@gmail.com>
2022-06-10 13:27:34 +0100

    Completion of https://docs.microsoft.com/en-us/learn/modules/fsharp-first-program/

---
+++ src/Errors/Program.fs
@@ -0,0 +1,4 @@
+﻿let aNumber = 0
+// below line has error FS0001: This expression was expected to have type↔    'string'    ↔but here has type↔    'int' [C:\git\F#-Learning\src\Errors\Errors.fsproj]
+// printfn "Here's a number %s" aNumber 
+printfn "Here's a number %i" aNumber
